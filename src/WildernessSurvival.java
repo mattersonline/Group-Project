2ff5453a6d54e192885baf5b88180d2b377c33b1
PSCapng <dtcomputerservice@gmail.com>
2015-03-25 22:10:41 -0500

    remove console output

--- src/WildernessSurvival.java
+++ src/WildernessSurvival.java
@@ -8,11 +8,7 @@ public class WildernessSurvival {
 	public static void main(String[] args) {
 		// initialize player
 		// initialize map
-		System.out.println("Testing the test map...");
 		TestMap map = new TestMap(10);
-		map.displayConsole();
-		System.out.println("Finished testing test map...");
-		System.out.println("Test finished");
 		// initialize gui
 		JFrame frame = new JFrame();
 		frame.setLayout(new BorderLayout());
