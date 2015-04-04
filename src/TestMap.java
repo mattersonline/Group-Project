9e886700c1f201710c993cb78cb458c52796f9ff
PSCapng <dtcomputerservice@gmail.com>
2015-04-03 23:57:35 -0500

    utilize setCard in the Map class instead of setting it's mapArray with an array that was passed in. This fixed issues with the scenario cards getting overwritten when the new array was passed in.

--- src/TestMap.java
+++ src/TestMap.java
@@ -11,24 +11,18 @@ public class TestMap extends Map
 		MapTile startTile = new MapTile(6,4);
 		MapTile finishTile = new MapTile(0,8);
 		MapTile currentPlayerLocation = new MapTile(6,4);
-		
-		Card[][] cards = new Card[mapSize][mapSize];
-		cards[startTile.getYCoordinate()][startTile.getXCoordinate()] = new StartCard();
-		cards[finishTile.getYCoordinate()][finishTile.getXCoordinate()] = new FinishCard();
-		
+				
 		// setup inaccessible areas
-		cards[3][0] = new InaccessibleAreaCard();
-		cards[3][1] = new InaccessibleAreaCard();
-		cards[2][4] = new InaccessibleAreaCard();
-		cards[3][4] = new InaccessibleAreaCard();
-		cards[4][4] = new InaccessibleAreaCard();
-		cards[5][5] = new InaccessibleAreaCard();
-		cards[6][6] = new InaccessibleAreaCard();
-		cards[7][5] = new InaccessibleAreaCard();
-		cards[8][7] = new InaccessibleAreaCard();
-		cards[9][8] = new InaccessibleAreaCard();
+		super.setCard(new MapTile(3, 1), new InaccessibleAreaCard());
+		super.setCard(new MapTile(2, 4), new InaccessibleAreaCard());
+		super.setCard(new MapTile(3, 4), new InaccessibleAreaCard());
+		super.setCard(new MapTile(4, 4), new InaccessibleAreaCard());
+		super.setCard(new MapTile(5, 5), new InaccessibleAreaCard());
+		super.setCard(new MapTile(6, 6), new InaccessibleAreaCard());
+		super.setCard(new MapTile(7, 5), new InaccessibleAreaCard());
+		super.setCard(new MapTile(8, 7), new InaccessibleAreaCard());
+		super.setCard(new MapTile(9, 8), new InaccessibleAreaCard());
 		
-		super.setCardArray(cards);
 		super.setFinish(finishTile);
 		super.setStart(startTile);
 		super.setCurrentPlayerLocation(currentPlayerLocation);
