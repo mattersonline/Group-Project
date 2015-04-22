ccf16f19421bd4ffaed1d9d62c8d63c90fdc830a
PSCapng <dtcomputerservice@gmail.com>
2015-04-21 22:08:31 -0500

    for testing purposes add code so if health or hunger run out it resets them

--- src/ScenarioDemo.java
+++ src/ScenarioDemo.java
@@ -10,9 +10,26 @@ public class ScenarioDemo extends situationScenario{
 		WildernessSurvival.gui.log("Sample script");
 		WildernessSurvival.gui.log("you are being attacked by a " + enemy.getClass().getTypeName());
 		enemy.attack(WildernessSurvival.player);
-		WildernessSurvival.gui.log("Your health is now : " + WildernessSurvival.player.getHealth());
-		WildernessSurvival.gui.log("Adding 20 Health");
-		WildernessSurvival.player.addHealth(20);
+		int newHealth = WildernessSurvival.player.getHealth();
+		int newHunger = WildernessSurvival.player.getEnergy();
+		WildernessSurvival.gui.log("Your health is now : " + newHealth);
+		WildernessSurvival.gui.log("Your hunger is now : " + newHunger);
+		
+		if(newHealth <= 0)
+		{
+			WildernessSurvival.gui.log("Your health is empty so resetting health");
+			WildernessSurvival.player.addHealth(100);
+			WildernessSurvival.gui.log("Your health is now : " + WildernessSurvival.player.getHealth());
+		}
+
+		if(newHunger <= 0)
+		{
+			WildernessSurvival.gui.log("Your hunger is empty so resetting health");
+			WildernessSurvival.player.addEnergy(100);
+			WildernessSurvival.gui.log("Your health is now : " + WildernessSurvival.player.getEnergy());
+		}
+		
+		WildernessSurvival.gui.updateHealthBar();
 	}
 	
 	public int getHealthMod(){
